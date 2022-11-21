c29eb9b31ff050f84df6523657b92dac1b7e4e85
Xelta69 <93995828+Xelta69@users.noreply.github.com>
2022-11-21 06:10:40 -0800

    Update Hospital_Manage.cpp

--- Hospital_Manage.cpp
+++ Hospital_Manage.cpp
@@ -1,6 +1,11 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 #include <string>
+#include <array>
+#include <cstdlib>
+#include <vector>
+#include <exception>
+
 using namespace std;
 
 class Formality{
@@ -18,18 +23,56 @@ class Formality{
         cout<<"What is your age? ";
         cin>>age;
     }
-}
+};
 
 class Doctors{
-    string doc_list[7] = {"Dr.Ahuja","Dr.Singh","Dr.Patel",
+    Doctors(){
+        srand(2);
+        week = rand() % 7;
+    }
+    vector <string> doc_list {"Dr.Ahuja","Dr.Singh","Dr.Patel",
                             "Dr.Khan", "Dr.Saxena","Dr.Nagraj",
                             "Dr.Gulati"};
-    int i = 0;
+    // int i = 7;
+    int week ;
+    
     protected:
-    void id_info(){
-        
+    
+    void get_doc(){
+        for(int i = 0; i < doc_list.size(); i++){
+            try{
+                if(week == i%7){
+                    cout<<doc_list[i]<<" "<<doc_list[i+1]
+                }
+            }
+            catch (out_of_range& e){
+                cout<<" ";
+            }
+                
+        }
     }
-}
+    
+    void get_weekday(int week_num){
+        switch(week_num){
+            case 0: cout<< "For Monday: ";break;
+            case 1: cout<< "For Tuesday: ";break;
+            case 2: cout<< "For Wednesday: ";break;
+            case 3: cout<< "For Thursday: ";break;
+            case 4: cout<< "For Friday: ";break;
+            case 5: cout<< "For Saturday: ";break;
+            case 6: cout<< "For Sunday: ";break;
+            default: cout<<"System error : Sorry For Inconvenience \n";break;
+        }
+    }
+    
+    void list_doc(){
+        for(week = 0; week < 7; week++){
+            get_weekday(week);
+            get_doc();
+            cout<<"\n";
+        }
+    }
+};
 
 int main() {
     
